0ecba8e534b827c47b93ad21c9480a20f98a56b4
naiderrp <136645587+naiderrp@users.noreply.github.com>
2024-05-17 07:31:49 -0700

    Simulation source code

--- src/direct-test/ComputeShader.hlsl
+++ src/direct-test/ComputeShader.hlsl
@@ -1,8 +1,8 @@
 struct Particle
 {
-    float3  pos;
-    float   radius;
-    float   opacity;
+    float3 pos;
+    float radius;
+    float opacity;
 };
 
 StructuredBuffer<Particle> sbParticles : register(t0);
@@ -10,10 +10,10 @@ RWBuffer<float> sbShadows : register(u0);
 
 cbuffer cbCS : register(b0)
 {
-    float3  sunDir;
+    float3 sunDir;
 }
 
-bool NeverIntersects(float3 particlePos, float3 otherParticlePos, float3 lightPos);
+bool NeverIntersects(float3 particlePos, float3 otherParticlePos);
 
 [numthreads(THREAD_X, THREAD_Y, 1)]
 void CSMain(uint3 groupID : SV_GroupID, uint3 dispatchID : SV_DispatchThreadID, uint3 groupThreadID : SV_GroupThreadID, uint groupIndex : SV_GroupIndex)
@@ -26,13 +26,11 @@ void CSMain(uint3 groupID : SV_GroupID, uint3 dispatchID : SV_DispatchThreadID, 
     {
         return;
     }
-        
-    float3 lightPos = sunDir;
+    
     Particle particle = sbParticles[index];
     
-    sbShadows[index] = particle.opacity;
+    sbShadows[index] = 1.0f;
     
-    float3 direction = normalize(lightPos - particle.pos); // direction from a particle to sun
     float3 origin = particle.pos;
         
     for (int i = 0; i < particlesCount; ++i)
@@ -44,18 +42,18 @@ void CSMain(uint3 groupID : SV_GroupID, uint3 dispatchID : SV_DispatchThreadID, 
         
         Particle otherParticle = sbParticles[i];
         
-        if (NeverIntersects(particle.pos, otherParticle.pos, lightPos))
+        if (NeverIntersects(particle.pos, otherParticle.pos))
         {
             continue;
         }
-
+        
         float x0 = otherParticle.pos.x;
         float y0 = otherParticle.pos.y;
         float z0 = otherParticle.pos.z;
         float radius = otherParticle.radius;
     
-        float a = pow(direction.x, 2) + pow(direction.y, 2) + pow(direction.z, 2);
-        float b = 2 * (direction.x * (origin.x - x0) + direction.y * (origin.y - y0) + direction.z * (origin.z - z0));
+        float a = pow(sunDir.x, 2) + pow(sunDir.y, 2) + pow(sunDir.z, 2);
+        float b = 2 * (sunDir.x * (origin.x - x0) + sunDir.y * (origin.y - y0) + sunDir.z * (origin.z - z0));
         float c = pow(origin.x - x0, 2) + pow(origin.y - y0, 2) + pow(origin.z - z0, 2) - pow(radius, 2);
     
         float D = b * b - 4 * a * c;
@@ -64,64 +62,51 @@ void CSMain(uint3 groupID : SV_GroupID, uint3 dispatchID : SV_DispatchThreadID, 
         {
             continue;
         }
-        sbShadows[index] *= 0.9;
+        
+        sbShadows[index] *= (1.0f - otherParticle.opacity);
     }
 }
 
-bool NeverIntersects(float3 particlePos, float3 otherParticlePos, float3 lightPos)
+bool NeverIntersects(float3 particlePos, float3 otherParticlePos)
 {
-    if (lightPos.x >= particlePos.x && lightPos.y <= particlePos.y && lightPos.z >= particlePos.z)
+    if (sunDir.x == 1.0f)
     {
-        if (otherParticlePos.x < particlePos.x || otherParticlePos.y > particlePos.y || otherParticlePos.z < particlePos.z)
+        if (otherParticlePos.x <= particlePos.x)
         {
             return true;
         }
     }
-    if (lightPos.x <= particlePos.x && lightPos.y <= particlePos.y && lightPos.z >= particlePos.z)
+    if (sunDir.x == -1.0f)
     {
-        if (otherParticlePos.x > particlePos.x || otherParticlePos.y > particlePos.y || otherParticlePos.z < particlePos.z)
+        if (otherParticlePos.x >= particlePos.x)
         {
             return true;
         }
     }
-    if (lightPos.x <= particlePos.x && lightPos.y >= particlePos.y && lightPos.z <= particlePos.z)
+    if (sunDir.y == 1.0f)
     {
-        if (otherParticlePos.x > particlePos.x || otherParticlePos.y < particlePos.y || otherParticlePos.z > particlePos.z)
+        if (otherParticlePos.y <= particlePos.y)
         {
             return true;
         }
     }
-    if (lightPos.x <= particlePos.x && lightPos.y <= particlePos.y && lightPos.z <= particlePos.z)
+    if (sunDir.y == -1.0f)
     {
-        if (otherParticlePos.x > particlePos.x || otherParticlePos.y > particlePos.y || otherParticlePos.z > particlePos.z)
+        if (otherParticlePos.y >= particlePos.y)
         {
             return true;
         }
     }
-    if (lightPos.x >= particlePos.x && lightPos.y >= particlePos.y && lightPos.z >= particlePos.z)
+    if (sunDir.z == 1.0f)
     {
-        if (otherParticlePos.x < particlePos.x || otherParticlePos.y < particlePos.y || otherParticlePos.z < particlePos.z)
+        if (otherParticlePos.z <= particlePos.z)
         {
             return true;
         }
     }
-    if (lightPos.x <= particlePos.x && lightPos.y >= particlePos.y && lightPos.z >= particlePos.z)
+    if (sunDir.z == -1.0f)
     {
-        if (otherParticlePos.x > particlePos.x || otherParticlePos.y < particlePos.y || otherParticlePos.z < particlePos.z)
-        {
-            return true;
-        }
-    }
-    if (lightPos.x >= particlePos.x && lightPos.y >= particlePos.y && lightPos.z <= particlePos.z)
-    {
-        if (otherParticlePos.x < particlePos.x || otherParticlePos.y < particlePos.y || otherParticlePos.z > particlePos.z)
-        {
-            return true;
-        }
-    }
-    if (lightPos.x >= particlePos.x && lightPos.y <= particlePos.y && lightPos.z <= particlePos.z)
-    {
-        if (otherParticlePos.x < particlePos.x || otherParticlePos.y > particlePos.y || otherParticlePos.z > particlePos.z)
+        if (otherParticlePos.z >= particlePos.z)
         {
             return true;
         }
